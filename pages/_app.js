3d640854a4f1d725aab7ca334e006e3faa19ed61
NICOLAS MAZZOLENI <mazzoleni.nicolas@gmail.com>
2023-02-22 11:33:30 +0100

    Code cleaning, adding the graph logic

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,12 @@ import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
 import Head from "next/head"
 import { NotificationProvider } from "web3uikit"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL,
+})
 
 function MyApp({ Component, pageProps }) {
     return (
@@ -12,11 +18,13 @@ function MyApp({ Component, pageProps }) {
                 <meta name="description" content="NFT Marketplace" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <MoralisProvider initializeOnMounts={false}>
-                <NotificationProvider>
-                    <Header />
-                    <Component {...pageProps} />
-                </NotificationProvider>
+            <MoralisProvider initializeOnMount={false}>
+                <ApolloProvider client={client}>
+                    <NotificationProvider>
+                        <Header />
+                        <Component {...pageProps} />
+                    </NotificationProvider>
+                </ApolloProvider>
             </MoralisProvider>
         </div>
     )
